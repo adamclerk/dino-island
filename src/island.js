c4e3324ecf35b87d89bd847bc4011850e966d129
Adam Clark <adamclerk@gmail.com>
2013-04-27 20:42:14 -0600

    Updating Island
    Islands can now...
    - kill dinos
    - generate islands from files
    - grow vegitation

--- src/island.js
+++ src/island.js
@@ -1,16 +1,55 @@
 var _ = require('underscore');
+var fs = require('fs');
 
 (function(exports, _){
 
-   	exports.island = function(){
+    var cord = function(x,y){
+        this.x = x;
+        this.y = y;
+        this.vegitation_calories = 0;
+    }
+
+    var gen_map = function(island_path){
+        var data = fs.readFileSync(island_path, 'utf8');
+        var data_arr = data.split("");
+        var row = 0;
+        var map = [];
+        var land = [];
+        map[0] = [];
+
+
+        for(var i = 0; i < data_arr.length; i++){
+            if(data_arr[i] == '\n'){
+                row++;
+                map[row] = [];
+            } else {
+                map[row].push(data_arr[i]);
+                if(data_arr[i] == '.'){
+                    land.push(new cord(row, i));
+                }
+            }
+        }
+        return {map: map, land:land};
+    }
+
+   	exports.island = function(island_path, vegitable_calories, percentage_plant, point_decay_per_turn){
+
+        if(!percentage_plant)percentage_plant = 10;
+        if(!point_decay_per_turn)point_decay_per_turn = 10;
+
         var island = {};
 
+        island.percentage_plant = percentage_plant;
+        island.point_decay_per_turn = point_decay_per_turn;
+        island.vegitable_calories = vegitable_calories;
+        island.map = gen_map(island_path);
         island.current_day = 1;
         island.dinos = [];
 
         island.add_dino = function(dino){
         	this.dinos.push(dino);
-        }
+        };
+
         island.next = function(){
         	var sortedDinos = _.sortBy(this.dinos, function(dino){return dino.speed});
         	var aliveDinos = _.where(sortedDinos, {is_alive:true});
@@ -21,13 +60,51 @@ var _ = require('underscore');
         	this.current_day++;
         };
 
-        island.dino_alive_count = function(){
-        	return _.where(this.dinos, {is_alive:true}).length;
+        island.grow = function(){
+            if(this.percentage_plant < 0){
+                return;
+            }
+
+            var new_vegitation_count = Math.ceil(this.map.land.length * this.percentage_plant);
+
+            for(var i = 0; i < new_vegitation_count; i++){
+                var randomland = this.map.land[Math.floor(Math.random() * this.map.land.length)];
+                randomland.vegitation_calories = randomland.vegitation_calories + this.vegitable_calories;
+            }
+
+            this.percentage_plant = this.percentage_plant - point_decay_per_turn;
         };
+
+        island.count = function(count){
+            switch(count){
+                case 'alive':
+                    return _.where(this.dinos, {is_alive:true}).length;
+                break;
+
+                case 'dead':
+                    return _.where(this.dinos, {is_alive:false}).length;
+                break;
+
+                case 'vegitation_calories':
+                    var sum = _.reduce(this.map.land, function(memo, land){ return memo + land.vegitation_calories; }, 0);
+                    return sum;
+                break;
+
+                case 'land':
+                    return this.map.land.length;
+                break;
+
+                default:
+                    return NaN;
+                break;
+            }
+        }
+
+
 
         island.dino_dead_count = function(){
         	return _.where(this.dinos, {is_alive:false}).length;
-        }
+        };
 
         return island;
     };
--- test/island_spec.js
+++ test/island_spec.js
@@ -6,13 +6,13 @@ describe('island', function() {
 	var i;
 
 	beforeEach(function(){
-        i = new Island();
+        i = new Island('./islands/lost.small.island', 50, .20, .20);
 	});
 
     it('should be span a dino', function(){
         var d = new Dino('trex', 'carnivore', 5,5,function(dino){})
         i.add_dino(d);
-        i.dino_alive_count().should.equal(1);
+        i.count('alive').should.equal(1);
     });
 
     it('should be go to next day', function(){
@@ -23,16 +23,29 @@ describe('island', function() {
     it('should be able to kill a lazy dino', function(){
         var d = new Dino('trex', 'carnivore', 5, 5, function(dino){})
         i.add_dino(d);
-        //i.dino_alive_count().should.equal(1);
-        //i.dino_dead_count().should.equal(0);
-
+        i.count('alive').should.equal(1);
+        i.count('dead').should.equal(0);
         for(var j = 0; j < 62; j++){
             i.next();
         }
+        i.count('alive').should.equal(0);
+        i.count('dead').should.equal(1);
+    });
 
-        i.dino_alive_count().should.equal(0);
-        i.dino_dead_count().should.equal(1);
+    it('should generate an island with land', function(){
+        i.map.should.not.be.null;
+        i.map.land.length.should.equal(180);
+        
+    });
 
+    it('should generate an island with a map', function(){
+        i.map.should.not.be.null;
+        i.map.map.length.should.equal(15);
+        i.map.map[0].length.should.equal(22);
+    });
 
+    it('should grow new vegitation', function(){
+        i.grow();
+        i.count('vegitation_calories').should.equal(1800);
     })
 });
